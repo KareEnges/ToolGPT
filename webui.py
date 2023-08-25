bf749e0e5fb44c54b959179ab9b891ca99773b63
EngelsVon <64879324+KareEnges@users.noreply.github.com>
2023-08-25 14:37:53 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -21,17 +21,32 @@ language = config.language
 
 
 def _main_():
+    if 'user' not in st.session_state:
+        st.session_state.user = []
+    if 'response' not in st.session_state:
+        st.session_state.response = []
     st.set_page_config(page_title="XZAITool", page_icon="logo.png")
-    st.title(config.uiTitle)
-    if tool_select := st.multiselect('Select tools', [i.name for i in main.tools], default=[i.name for i in main.tools]):
+    st.title(title)
+    if tool_select := st.multiselect('Select tools', [i.name for i in main.tools],
+                                     default=[i.name for i in main.tools]):
         init(tool_select)
     if st.button("清空记忆"):
         main.memory.clear()
+        st.session_state.response = []
+        st.session_state.user = []
+    for i in range(len(st.session_state.user) if len(st.session_state.user) >= len(st.session_state.response) else len(st.session_state.response)):
+        try:
+            st.chat_message("user").write(st.session_state.user[i])
+            st.chat_message("assistant").write(st.session_state.response[i])
+        except:
+            pass
     if prompt := st.chat_input():
+        st.session_state.user.append(prompt)
         st.chat_message("user").write(prompt)
         with st.chat_message("assistant"):
             st_callback = StreamlitCallbackHandler(st.container())
             response = agent.run(input=prompt + "answer with language:" + language, callbacks=[st_callback])
+            st.session_state.response.append(response)
             st.write(response)
 
 
